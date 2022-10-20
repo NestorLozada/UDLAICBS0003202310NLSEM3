e75cac833d8895827512904b086ead6f2a0b4ce7
NestorLozada <martinlozada1@gmail.com>
2022-10-20 00:44:31 -0500

    adding extract files

---
+++ extract/chanels_for_ext.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sqlalchemy.engine import Engine
+from config import DataConfig
+
+def extract_channels(db_con: Engine):
+    #Dictionary for values
+    channels_dict = {
+        "channel_id": [],
+        "channel_desc": [],
+        "channel_class": [],
+        "channel_class_id": []
+    }
+    channels_csv = pd.read_csv(DataConfig.get_csv_path('channels.csv'))
+    
+    #Process CSV Content
+    if not channels_csv.empty:
+        for id,desc,cls,cls_id \
+            in zip(
+                channels_csv['CHANNEL_ID'],
+                channels_csv['CHANNEL_DESC'],
+                channels_csv['CHANNEL_CLASS'],
+                channels_csv['CHANNEL_CLASS_ID']
+            ):
+            channels_dict["channel_id"].append(id)
+            channels_dict["channel_desc"].append(desc)
+            channels_dict["channel_class"].append(cls)
+            channels_dict["channel_class_id"].append(cls_id)
+    if channels_dict["channel_id"]:
+        db_con.connect().execute(f'TRUNCATE TABLE channels')
+        df_countries = pd.DataFrame(channels_dict)
+        df_countries.to_sql('channels', db_con, if_exists="append",index=False)
--- utilsconnection/db_connection.py
+++ utilsconnection/db_connection.py
@@ -1,7 +1,56 @@
 from sqlalchemy import create_engine
-class Db_Connection():
-    def __init__(self, type, host, port, user, password, database):        
+import traceback
 
+class DbConnection():
+    """
+    Class to manage database connections
+    Attributes
+    ----------
+        connection : SQLAlchemy Engine
+            database connection pool
+        type : str, required
+            type of database
+        host : str, required
+            database host
+        port : str, required
+            database port
+        user : str, required
+            database username
+        password : str, required
+            database password
+        database : str, required
+            name of the database
+    Methods
+    -------
+    start(self):
+        Creates and returns a database connection pool.
+    stop(self):
+        Dispose the database connection pool.    
+    """
+
+    def __init__(self, type, host, port, user, password, database):        
+        """Constructor with initialization values for the class attributes
+        Parameters
+        ----------
+        type : str, required
+            value for the type attribute
+        host : str, required
+            value for the host attribute
+        port : str, required
+            value for the port attribute
+        user : str, required
+            value for the user attribute
+        password : str, required
+            value for the password attribute
+        database : str, required
+            value for the database attribute
+        Returns
+        -------
+        None
+        Raises
+        ------
+        None
+        """    
         self.connection = None
         self.type = type
         self.host = host
@@ -11,7 +60,19 @@ class Db_Connection():
         self.database = database
 
     def start(self):
-      
+        """Create and returns a database connection.
+        Parameters
+        ----------
+        None      
+        
+        Returns
+        -------
+        SQLAlchemy Engine
+            database connection pool
+        Raises
+        ------
+        None
+        """          
         try:
             if self.type == 'mysql':
                 db_connection_str = 'mysql+pymysql://'+self.user+':'+self.password+'@'+self.host+':'+self.port+'/'+self.database
